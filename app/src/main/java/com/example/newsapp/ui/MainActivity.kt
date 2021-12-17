d7f897c2e4a477b6eb77cc2b9fd09398ffb4e495
bhaveshRepo <sutharBhavesh@outlook.in>
2021-12-17 18:29:52 +0530

    going to be pushed to github using Command Line_1

--- app/src/main/java/com/example/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         println("This is experimental commit")
         println("This is branch 3")
         println("Edited after adding repo to Github")
+        println("Pushed using commandline")
 
 
     }
